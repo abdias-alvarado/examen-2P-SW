e40ab13772cc49fe30c6995d763da05c25e6f1ea
abdias-alvarado <abdias.alvarado@unah.hn>
2019-03-12 19:32:37 -0600

    Agregando las proyecciones.

--- routes/api/employeeModel.js
+++ routes/api/employeeModel.js
@@ -22,20 +22,23 @@ function employeeModel(db){
     // implementar
     // Obtener un Documento solo mostrar
     // email, phone, name y age
-    empColl.findOne({ "_id": ObjectID(id)}, (err, result)=>{
+    empColl.find({"_id": ObjectID(id)}).project({'email':1, 'phone':1, 'name':1, 'age':1}).toArray(
+      (err , resultado) => {
         if(err){
           handler(err, null);
         }else{
-          handler(null, result);
+          handler(null, resultado);
         }
-      });
+      }
+     );
+
     //return handler(new Error("No Implementado"), null);
   }
 
   lib.getEmployeesByCompany = (company, handler) => {
     // implementar
     // solo mostrar name, email, company
-    empColl.find({"company": company}).toArray(
+    empColl.find({"company": company}).project({'name':1, 'email':1, 'company':1}).toArray(
       (err , resultado) => {
         if(err){
           handler(err, null);
@@ -54,8 +57,7 @@ function employeeModel(db){
     // que esten entre las edades indicadas por los parametros
     // ageLowLimit y ageHighLimit
     // solo mostrar name, age, email
-   console.log(ageLowLimit+ageHighLimit);
-    empColl.find({"age": { "$gte": parseInt(ageLowLimit), "$lte": parseInt(ageHighLimit)}}).toArray(
+    empColl.find({"age": { "$gte": parseInt(ageLowLimit), "$lte": parseInt(ageHighLimit)}}).project({'name':1, 'age':1, 'email':1}).toArray(
       (err , resultado) => {
         if(err){
           handler(err, null);
